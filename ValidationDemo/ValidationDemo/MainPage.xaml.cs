857b2c77451b10d338924da3c1af9b9d505cb19b
dallynowens2 <dallynowens2@gmail.com>
2021-04-16 13:36:15 -0600

    Add project files.

---
+++ ValidationDemo/ValidationDemo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ValidationDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool _emailValid;
+
+        public bool EmailValid
+        {
+            get => _emailValid;
+            set
+            {
+                _emailValid = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
